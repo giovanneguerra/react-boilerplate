5f00ca852236b7737742f8d9133fc5f33ac40fb9
Giovanne Guerra <giovanne.guerra@fitbank.com.br>
2019-03-21 09:31:24 -0300

    changed the folders structures

--- src/scripts/contents/App.js
+++ src/scripts/contents/App.js
@@ -1,16 +1,12 @@
-import React, {Component, Fragment} from 'react';
-import Button from "./button/button";
-import styles from './styles/main.scss';
-import FancyButton from "./fancy-button/fancy-button";
+import React from 'react';
+import styles from '../../styles/scss/main.scss';
 
 const content = 'Hello world!';
 
-const App = () => (
-  <Fragment>
-    <div className={styles["resizeable-both"]}>{content}</div>
-    <Button label="Regular Button"/>
-    <FancyButton label="Fancy Button"/>
-  </Fragment>
-);
+const App = () => {
+  return(
+    <div className={styles}>{content}</div>
+  );
+}
 
 export default App;
